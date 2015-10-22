7cedb5e064324d2db1f324ee6fa9f1aa1fce611d
Bin Hwang <hwangbin@msu.edu>
2015-10-22 13:47:50 -0400

    test ipynb

---
+++ qoct.py
@@ -0,0 +1,22 @@
+import numpy as np
+# Editor Bin H.
+# Quantum Optimal Control Example
+# Two level system
+
+class qoct:
+
+	def __init__(self,H0,Hctr):
+	
+		
+	
+
+	def run(self):
+		
+		a=1
+		return a 	
+
+
+if __name__ == '__main__':
+
+	
+		
